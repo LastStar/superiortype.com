81d7a6bb7f21dc897ffd988d21f9933555087181
Josef Pospíšil <josef.pospisil@laststar.eu>
2015-06-02 12:03:36 +0200

    Site updated to cd83fb4

--- assets/javascripts/app-61e68af8.js
+++ assets/javascripts/app-61e68af8.js
@@ -35,7 +35,7 @@ if(1==arguments.length){if(d.is(a,"object")){for(var f in a)a[t](f)&&this.data(f
 
 ;(function(l){'use strict';l(['jquery'],function($){var k=$.scrollTo=function(a,b,c){return $(window).scrollTo(a,b,c)};k.defaults={axis:'xy',duration:0,limit:true};function isWin(a){return!a.nodeName||$.inArray(a.nodeName.toLowerCase(),['iframe','#document','html','body'])!==-1}$.fn.scrollTo=function(f,g,h){if(typeof g==='object'){h=g;g=0}if(typeof h==='function'){h={onAfter:h}}if(f==='max'){f=9e9}h=$.extend({},k.defaults,h);g=g||h.duration;var j=h.queue&&h.axis.length>1;if(j){g/=2}h.offset=both(h.offset);h.over=both(h.over);return this.each(function(){if(f===null)return;var d=isWin(this),elem=d?this.contentWindow||window:this,$elem=$(elem),targ=f,attr={},toff;switch(typeof targ){case'number':case'string':if(/^([+-]=?)?\d+(\.\d+)?(px|%)?$/.test(targ)){targ=both(targ);break}targ=d?$(targ):$(targ,elem);if(!targ.length)return;case'object':if(targ.is||targ.style){toff=(targ=$(targ)).offset()}}var e=$.isFunction(h.offset)&&h.offset(elem,targ)||h.offset;$.each(h.axis.split(''),function(i,a){var b=a==='x'?'Left':'Top',pos=b.toLowerCase(),key='scroll'+b,prev=$elem[key](),max=k.max(elem,a);if(toff){attr[key]=toff[pos]+(d?0:prev-$elem.offset()[pos]);if(h.margin){attr[key]-=parseInt(targ.css('margin'+b),10)||0;attr[key]-=parseInt(targ.css('border'+b+'Width'),10)||0}attr[key]+=e[pos]||0;if(h.over[pos]){attr[key]+=targ[a==='x'?'width':'height']()*h.over[pos]}}else{var c=targ[pos];attr[key]=c.slice&&c.slice(-1)==='%'?parseFloat(c)/100*max:c}if(h.limit&&/^\d+$/.test(attr[key])){attr[key]=attr[key]<=0?0:Math.min(attr[key],max)}if(!i&&h.axis.length>1){if(prev===attr[key]){attr={}}else if(j){animate(h.onAfterFirst);attr={}}}});animate(h.onAfter);function animate(a){var b=$.extend({},h,{queue:true,duration:g,complete:a&&function(){a.call(elem,targ,h)}});$elem.animate(attr,b)}})};k.max=function(a,b){var c=b==='x'?'Width':'Height',scroll='scroll'+c;if(!isWin(a))return a[scroll]-$(a)[c.toLowerCase()]();var d='client'+c,doc=a.ownerDocument||a.document,html=doc.documentElement,body=doc.body;return Math.max(html[scroll],body[scroll])-Math.min(html[d],body[d])};function both(a){return $.isFunction(a)||$.isPlainObject(a)?a:{top:a,left:a}}$.Tween.propHooks.scrollLeft=$.Tween.propHooks.scrollTop={get:function(t){return $(t.elem)[t.prop]()},set:function(t){var a=this.get(t);if(t.options.interrupt&&t._last&&t._last!==a){return $(t.elem).stop()}var b=Math.round(t.now);if(a!==b){$(t.elem)[t.prop](b);t._last=this.get(t)}}};return k})}(typeof define==='function'&&define.amd?define:function(a,b){'use strict';if(typeof module!=='undefined'&&module.exports){module.exports=b(require('jquery'))}else{b(jQuery)}}));
 (function() {
-  var buttons, config, height, maxWidth, moveButton, restartDelay, returnButtons, scale, showFontList, snap, svg, width, zero;
+  var buttons, config, height, maxWidth, moveButton, restartDelay, returnButtons, scale, snap, svg, width, zero;
 
   if ($('#show-room').size() > 0) {
     svg = $('svg');
@@ -213,7 +213,7 @@ if(1==arguments.length){if(d.is(a,"object")){for(var f in a)a[t](f)&&this.data(f
     };
   }
 
-  showFontList = function() {
+  if ($('section.fonts').size() > 0) {
     $('.tools .minus').on('click', function() {
       var family, familySize, styles, stylesSize;
       family = $(this).parent().siblings('h3');
@@ -246,12 +246,29 @@ if(1==arguments.length){if(d.is(a,"object")){for(var f in a)a[t](f)&&this.data(f
       styles.addClass('visible');
       return $.scrollTo(styles, 'max');
     });
-    return $('.fonts').css({
+    $('.fonts').css({
       opacity: 1
     });
-  };
+  }
 
-  showFontList();
+  if ($('section.fonts#styles').size() > 0) {
+    $('.tools .minus').on('click', function() {
+      var styles, stylesSize;
+      styles = $(this).parent().siblings('h4');
+      stylesSize = (parseInt(styles.first().css('font-size')) * 0.875) + 'px';
+      return styles.css({
+        'font-size': stylesSize
+      });
+    });
+    $('.tools .plus').on('click', function() {
+      var styles, stylesSize;
+      styles = $(this).parent().siblings('h4');
+      stylesSize = (parseInt(styles.first().css('font-size')) * 1.125) + 'px';
+      return styles.css({
+        'font-size': stylesSize
+      });
+    });
+  }
 
   $('header.main').on('mouseenter', function() {
     return $('header.main nav').addClass('visible');
