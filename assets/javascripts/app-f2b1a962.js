d29ccc6f91043c909d83aa5e50dc68a35cd72516
Josef Pospíšil <josef.pospisil@laststar.eu>
2015-06-10 11:16:12 +0200

    Site updated to 693cfa2

--- assets/javascripts/app-f2b1a962.js
+++ assets/javascripts/app-f2b1a962.js
@@ -38,7 +38,7 @@ if(1==arguments.length){if(d.is(a,"object")){for(var f in a)a[t](f)&&this.data(f
 
 !function(e){"function"==typeof define&&define.amd?define(["jquery"],e):e("object"==typeof exports?require("jquery"):jQuery)}(function(e){function t(t){var r,i,n,o=arguments.length,s=window[t],a=arguments,u=a[1];if(2>o)throw Error("Minimum 2 arguments must be given");if(e.isArray(u)){i={};for(var f in u){r=u[f];try{i[r]=JSON.parse(s.getItem(r))}catch(c){i[r]=s.getItem(r)}}return i}if(2!=o){try{i=JSON.parse(s.getItem(u))}catch(c){throw new ReferenceError(u+" is not defined in this storage")}for(var f=2;o-1>f;f++)if(i=i[a[f]],void 0===i)throw new ReferenceError([].slice.call(a,1,f+1).join(".")+" is not defined in this storage");if(e.isArray(a[f])){n=i,i={};for(var m in a[f])i[a[f][m]]=n[a[f][m]];return i}return i[a[f]]}try{return JSON.parse(s.getItem(u))}catch(c){return s.getItem(u)}}function r(t){var r,i,n=arguments.length,o=window[t],s=arguments,a=s[1],u=s[2],f={};if(2>n||!e.isPlainObject(a)&&3>n)throw Error("Minimum 3 arguments must be given or second parameter must be an object");if(e.isPlainObject(a)){for(var c in a)r=a[c],e.isPlainObject(r)?o.setItem(c,JSON.stringify(r)):o.setItem(c,r);return a}if(3==n)return"object"==typeof u?o.setItem(a,JSON.stringify(u)):o.setItem(a,u),u;try{i=o.getItem(a),null!=i&&(f=JSON.parse(i))}catch(m){}i=f;for(var c=2;n-2>c;c++)r=s[c],i[r]&&e.isPlainObject(i[r])||(i[r]={}),i=i[r];return i[s[c]]=s[c+1],o.setItem(a,JSON.stringify(f)),f}function i(t){var r,i,n=arguments.length,o=window[t],s=arguments,a=s[1];if(2>n)throw Error("Minimum 2 arguments must be given");if(e.isArray(a)){for(var u in a)o.removeItem(a[u]);return!0}if(2==n)return o.removeItem(a),!0;try{r=i=JSON.parse(o.getItem(a))}catch(f){throw new ReferenceError(a+" is not defined in this storage")}for(var u=2;n-1>u;u++)if(i=i[s[u]],void 0===i)throw new ReferenceError([].slice.call(s,1,u).join(".")+" is not defined in this storage");if(e.isArray(s[u]))for(var c in s[u])delete i[s[u][c]];else delete i[s[u]];return o.setItem(a,JSON.stringify(r)),!0}function n(t,r){var n=a(t);for(var o in n)i(t,n[o]);if(r)for(var o in e.namespaceStorages)u(o)}function o(r){var i=arguments.length,n=arguments,s=(window[r],n[1]);if(1==i)return 0==a(r).length;if(e.isArray(s)){for(var u=0;u<s.length;u++)if(!o(r,s[u]))return!1;return!0}try{var f=t.apply(this,arguments);e.isArray(n[i-1])||(f={totest:f});for(var u in f)if(!(e.isPlainObject(f[u])&&e.isEmptyObject(f[u])||e.isArray(f[u])&&!f[u].length)&&f[u])return!1;return!0}catch(c){return!0}}function s(r){var i=arguments.length,n=arguments,o=(window[r],n[1]);if(2>i)throw Error("Minimum 2 arguments must be given");if(e.isArray(o)){for(var a=0;a<o.length;a++)if(!s(r,o[a]))return!1;return!0}try{var u=t.apply(this,arguments);e.isArray(n[i-1])||(u={totest:u});for(var a in u)if(void 0===u[a]||null===u[a])return!1;return!0}catch(f){return!1}}function a(r){var i=arguments.length,n=window[r],o=arguments,s=(o[1],[]),a={};if(a=i>1?t.apply(this,o):n,a._cookie)for(var u in e.cookie())""!=u&&s.push(u.replace(a._prefix,""));else for(var f in a)s.push(f);return s}function u(t){if(!t||"string"!=typeof t)throw Error("First parameter must be a string");g?(window.localStorage.getItem(t)||window.localStorage.setItem(t,"{}"),window.sessionStorage.getItem(t)||window.sessionStorage.setItem(t,"{}")):(window.localCookieStorage.getItem(t)||window.localCookieStorage.setItem(t,"{}"),window.sessionCookieStorage.getItem(t)||window.sessionCookieStorage.setItem(t,"{}"));var r={localStorage:e.extend({},e.localStorage,{_ns:t}),sessionStorage:e.extend({},e.sessionStorage,{_ns:t})};return e.cookie&&(window.cookieStorage.getItem(t)||window.cookieStorage.setItem(t,"{}"),r.cookieStorage=e.extend({},e.cookieStorage,{_ns:t})),e.namespaceStorages[t]=r,r}function f(e){var t="jsapi";try{return window[e]?(window[e].setItem(t,t),window[e].removeItem(t),!0):!1}catch(r){return!1}}var c="ls_",m="ss_",g=f("localStorage"),l={_type:"",_ns:"",_callMethod:function(e,t){var r=[this._type],t=Array.prototype.slice.call(t),i=t[0];return this._ns&&r.push(this._ns),"string"==typeof i&&-1!==i.indexOf(".")&&(t.shift(),[].unshift.apply(t,i.split("."))),[].push.apply(r,t),e.apply(this,r)},get:function(){return this._callMethod(t,arguments)},set:function(){var t=arguments.length,i=arguments,n=i[0];if(1>t||!e.isPlainObject(n)&&2>t)throw Error("Minimum 2 arguments must be given or first parameter must be an object");if(e.isPlainObject(n)&&this._ns){for(var o in n)r(this._type,this._ns,o,n[o]);return n}var s=this._callMethod(r,i);return this._ns?s[n.split(".")[0]]:s},remove:function(){if(arguments.length<1)throw Error("Minimum 1 argument must be given");return this._callMethod(i,arguments)},removeAll:function(e){return this._ns?(r(this._type,this._ns,{}),!0):n(this._type,e)},isEmpty:function(){return this._callMethod(o,arguments)},isSet:function(){if(arguments.length<1)throw Error("Minimum 1 argument must be given");return this._callMethod(s,arguments)},keys:function(){return this._callMethod(a,arguments)}};if(e.cookie){window.name||(window.name=Math.floor(1e8*Math.random()));var h={_cookie:!0,_prefix:"",_expires:null,_path:null,_domain:null,setItem:function(t,r){e.cookie(this._prefix+t,r,{expires:this._expires,path:this._path,domain:this._domain})},getItem:function(t){return e.cookie(this._prefix+t)},removeItem:function(t){return e.removeCookie(this._prefix+t)},clear:function(){for(var t in e.cookie())""!=t&&(!this._prefix&&-1===t.indexOf(c)&&-1===t.indexOf(m)||this._prefix&&0===t.indexOf(this._prefix))&&e.removeCookie(t)},setExpires:function(e){return this._expires=e,this},setPath:function(e){return this._path=e,this},setDomain:function(e){return this._domain=e,this},setConf:function(e){return e.path&&(this._path=e.path),e.domain&&(this._domain=e.domain),e.expires&&(this._expires=e.expires),this},setDefaultConf:function(){this._path=this._domain=this._expires=null}};g||(window.localCookieStorage=e.extend({},h,{_prefix:c,_expires:3650}),window.sessionCookieStorage=e.extend({},h,{_prefix:m+window.name+"_"})),window.cookieStorage=e.extend({},h),e.cookieStorage=e.extend({},l,{_type:"cookieStorage",setExpires:function(e){return window.cookieStorage.setExpires(e),this},setPath:function(e){return window.cookieStorage.setPath(e),this},setDomain:function(e){return window.cookieStorage.setDomain(e),this},setConf:function(e){return window.cookieStorage.setConf(e),this},setDefaultConf:function(){return window.cookieStorage.setDefaultConf(),this}})}e.initNamespaceStorage=function(e){return u(e)},g?(e.localStorage=e.extend({},l,{_type:"localStorage"}),e.sessionStorage=e.extend({},l,{_type:"sessionStorage"})):(e.localStorage=e.extend({},l,{_type:"localCookieStorage"}),e.sessionStorage=e.extend({},l,{_type:"sessionCookieStorage"})),e.namespaceStorages={},e.removeAllStorages=function(t){e.localStorage.removeAll(t),e.sessionStorage.removeAll(t),e.cookieStorage&&e.cookieStorage.removeAll(t),t||(e.namespaceStorages={})}});
 (function() {
-  var addToLiked, currentLiked, family, fixHeader, hideLikedBox, inLiked, itemFromLike, likedBox, likedClose, likedSpan, refreshLiked, removeFromLiked, renderLiked, showLikedBox, showSlideShow, styles, stylesGr;
+  var addToLiked, currentLiked, family, fixHeader, hideLikedBox, inLiked, likedBox, likedClose, likedSpan, refreshLiked, removeFromLiked, renderLiked, showLikedBox, showSlideShow, styles, stylesGr;
 
   likedSpan = $('#liked span');
 
@@ -100,10 +100,6 @@ if(1==arguments.length){if(d.is(a,"object")){for(var f in a)a[t](f)&&this.data(f
     }
   };
 
-  itemFromLike = function(el) {
-    return el.parents('.tools').siblings('h4').children('input');
-  };
-
   currentLiked = function() {
     return $.localStorage.get('liked');
   };
@@ -143,11 +139,11 @@ if(1==arguments.length){if(d.is(a,"object")){for(var f in a)a[t](f)&&this.data(f
       name = $(this).data('name');
       removeFromLiked(name);
       $(this).parent('li').fadeOut();
+      $(".like[data-name='" + name + "']").removeClass('pushed');
       if (currentLiked().length > 0) {
         refreshLiked(currentLiked());
         return renderLiked();
       } else {
-        refreshLiked(currentLiked());
         return hideLikedBox();
       }
     });
@@ -157,37 +153,43 @@ if(1==arguments.length){if(d.is(a,"object")){for(var f in a)a[t](f)&&this.data(f
     $.localStorage.set('liked', []);
   } else {
     $.each($('.like'), function(index, button) {
-      if (inLiked(itemFromLike($(button)).data('name'))) {
-        return $(button).hide();
+      if (inLiked($(button).data('name'))) {
+        return $(button).addClass('pushed');
       }
     });
     refreshLiked(currentLiked());
   }
 
   showSlideShow = function() {
-    var buttons, config, height, maxWidth, moveButton, restartDelay, returnButtons, scale, snap, svg, width, zero;
+    var buttons, config, halfCircle, height, moveButton, restartDelay, returnButtons, scale, snap, svg, width, zero;
+    if ($('#show-room').size() === 0) {
+      return false;
+    }
     svg = $('svg');
-    maxWidth = svg.width();
-    width = svg.width() - 150;
-    height = svg.height();
+    height = $(window).height() - 53;
+    width = svg.width();
+    svg.css({
+      height: height
+    });
     snap = Snap("#show-room");
     snap.clear();
     zero = 'translate(0, 0)';
     scale = {
-      initial: 0.01,
-      final: 1.9
+      initial: 0.0001,
+      final: $(window).height() / 550
     };
+    halfCircle = 110 * scale.final;
     config = {
       hrot: {
         id: 'hrot',
-        initial: [(width / 2) - 5, -76],
-        final: [(width / 2) - 145, 80],
+        initial: [(width / 2) - 5, 0],
+        final: [(width / 2) - halfCircle, height / 7],
         speed: 900
       },
       kundaBook: {
         id: 'kunda-book',
         initial: [width + 90, height],
-        final: [width / 2 + 70, 440],
+        final: [width / 2, height / 2],
         speed: 800,
         showRoom: '/assets/images/kunda-book-show-room.svg',
         preSlide: function(show) {
@@ -198,7 +200,7 @@ if(1==arguments.length){if(d.is(a,"object")){for(var f in a)a[t](f)&&this.data(f
             opacity: 0
           });
           return show.select('g#background rect').attr({
-            width: maxWidth,
+            width: width,
             height: height,
             opacity: 0
           });
@@ -238,7 +240,7 @@ if(1==arguments.length){if(d.is(a,"object")){for(var f in a)a[t](f)&&this.data(f
       veganSans: {
         id: 'vegan-sans',
         initial: [0, height],
-        final: [width / 2 - 370, 440],
+        final: [(width / 2) - 2 * halfCircle, height / 2],
         speed: 700,
         showRoom: '/assets/images/vegan-sans-show-room.svg',
         color: '#ff0',
@@ -275,6 +277,7 @@ if(1==arguments.length){if(d.is(a,"object")){for(var f in a)a[t](f)&&this.data(f
         }
       }
     };
+    console.log(config);
     restartDelay = 3000;
     buttons = null;
     Snap.load('/assets/images/buttons.svg', function(canvas) {
@@ -458,11 +461,11 @@ if(1==arguments.length){if(d.is(a,"object")){for(var f in a)a[t](f)&&this.data(f
     });
   }
 
-  $('header.main').on('mouseenter', function() {
+  $('header.main h1').on('mouseenter', function() {
     return $('header.main nav').addClass('visible');
   });
 
-  $('header.main').on('mouseout', function() {
+  $('header.main h1').on('mouseout', function() {
     var hideMenu;
     hideMenu = function() {
       return $('header.main nav').removeClass('visible');
@@ -490,7 +493,7 @@ if(1==arguments.length){if(d.is(a,"object")){for(var f in a)a[t](f)&&this.data(f
   if ($('.like').size() > 0) {
     $('.like').on('click', function() {
       var name;
-      name = itemFromLike($(this)).data('name');
+      name = $(this).data('name');
       addToLiked(name);
       $(this).addClass('pushed');
       renderLiked();
@@ -500,6 +503,7 @@ if(1==arguments.length){if(d.is(a,"object")){for(var f in a)a[t](f)&&this.data(f
 
   $('.remove-all').on('click', function() {
     $.localStorage.removeAll();
+    $('.pushed').removeClass('pushed');
     $.localStorage.set('liked', []);
     $('ul.items').html('');
     refreshLiked(currentLiked());
