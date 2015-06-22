fa040be05511b85a70110372c90dcedd9aa38aa7
Josef Pospíšil <josef.pospisil@laststar.eu>
2015-06-22 14:54:54 +0200

    Site updated to c03e8ef

--- assets/javascripts/app-021cd5eb.js
+++ assets/javascripts/app-021cd5eb.js
@@ -52,103 +52,7 @@ Licensed under the MIT license.
 https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
 */
 
-(function() {
-  'use strict'
-
-  function noop() {}
-
-  var Waypoint = window.Waypoint
-
-  /* http://imakewebthings.com/waypoints/shortcuts/inview */
-  function Inview(options) {
-    this.options = Waypoint.Adapter.extend({}, Inview.defaults, options)
-    this.axis = this.options.horizontal ? 'horizontal' : 'vertical'
-    this.waypoints = []
-    this.createWaypoints()
-  }
-
-  /* Private */
-  Inview.prototype.createWaypoints = function() {
-    var configs = {
-      vertical: [{
-        down: 'enter',
-        up: 'exited',
-        offset: '100%'
-      }, {
-        down: 'entered',
-        up: 'exit',
-        offset: 'bottom-in-view'
-      }, {
-        down: 'exit',
-        up: 'entered',
-        offset: 0
-      }, {
-        down: 'exited',
-        up: 'enter',
-        offset: function() {
-          return -this.adapter.outerHeight()
-        }
-      }],
-      horizontal: [{
-        right: 'enter',
-        left: 'exited',
-        offset: '100%'
-      }, {
-        right: 'entered',
-        left: 'exit',
-        offset: 'right-in-view'
-      }, {
-        right: 'exit',
-        left: 'entered',
-        offset: 0
-      }, {
-        right: 'exited',
-        left: 'enter',
-        offset: function() {
-          return -this.adapter.outerWidth()
-        }
-      }]
-    }
-
-    for (var i = 0, end = configs[this.axis].length; i < end; i++) {
-      var config = configs[this.axis][i]
-      this.createWaypoint(config)
-    }
-  }
-
-  /* Private */
-  Inview.prototype.createWaypoint = function(config) {
-    var self = this
-    this.waypoints.push(new Waypoint({
-      element: this.options.element,
-      handler: (function(config) {
-        return function(direction) {
-          self.options[config[direction]].call(this, direction)
-        }
-      }(config)),
-      offset: config.offset,
-      horizontal: this.options.horizontal
-    }))
-  }
-
-  /* Public */
-  Inview.prototype.destroy = function() {
-    for (var i = 0, end = this.waypoints.length; i < end; i++) {
-      this.waypoints[i].destroy()
-    }
-    this.waypoints = []
-  }
-
-  Inview.defaults = {
-    enter: noop,
-    entered: noop,
-    exit: noop,
-    exited: noop
-  }
-
-  Waypoint.Inview = Inview
-}())
-;
+!function(){"use strict";function t(){}function e(t){this.options=i.Adapter.extend({},e.defaults,t),this.axis=this.options.horizontal?"horizontal":"vertical",this.waypoints=[],this.createWaypoints()}var i=window.Waypoint;e.prototype.createWaypoints=function(){for(var t={vertical:[{down:"enter",up:"exited",offset:"100%"},{down:"entered",up:"exit",offset:"bottom-in-view"},{down:"exit",up:"entered",offset:0},{down:"exited",up:"enter",offset:function(){return-this.adapter.outerHeight()}}],horizontal:[{right:"enter",left:"exited",offset:"100%"},{right:"entered",left:"exit",offset:"right-in-view"},{right:"exit",left:"entered",offset:0},{right:"exited",left:"enter",offset:function(){return-this.adapter.outerWidth()}}]},e=0,i=t[this.axis].length;i>e;e++){var o=t[this.axis][e];this.createWaypoint(o)}},e.prototype.createWaypoint=function(t){var e=this;this.waypoints.push(new i({element:this.options.element,handler:function(t){return function(i){e.options[t[i]].call(this,i)}}(t),offset:t.offset,horizontal:this.options.horizontal}))},e.prototype.destroy=function(){for(var t=0,e=this.waypoints.length;e>t;t++)this.waypoints[t].destroy();this.waypoints=[]},e.defaults={enter:t,entered:t,exit:t,exited:t},i.Inview=e}();
 (function() {
   var addToWished, address, clearMessage, currentWished, defaultSpeed, detailsActive, detailsIn, emailIsValid, family, fixHeader, glyphsActive, glyphsIn, glyphsSelect, hideWishedBox, inWished, refreshWished, removeFromWished, removed, renderWished, showSlideShow, showWishedBox, style, styles, stylesActive, stylesGr, stylesIn, wishedBox, wishedClose, wishedSpan;
 
@@ -386,11 +290,14 @@ https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
         var timeout;
         timeout = setTimeout(restart, restartDelay);
         return elementToMove.mouseover(function() {
-          var wish;
+          var name, wish;
           elementToMove.unmouseover();
           clearTimeout(timeout);
           moveToPosition(title, item.final, scale.final);
           wish = title.select('#wish');
+          wish.attr({
+            cursor: 'pointer'
+          });
           wish.click(function() {
             addToWished(item.name);
             refreshWished(currentWished());
@@ -398,6 +305,13 @@ https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
             moveToPosition(title, item.initial, scale.initial);
             setTimeout(restart, defaultSpeed);
             return wish.unclick();
+          });
+          name = title.select('#name');
+          name.attr({
+            cursor: 'pointer'
+          });
+          name.click(function() {
+            return document.location = '/fonts/' + item.id;
           });
           return title.click(function() {
             moveToPosition(title, item.initial, scale.initial);
@@ -682,8 +596,8 @@ https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
 
   if ($('select.glyphs').size() > 0) {
     glyphsSelect = $('select.glyphs');
-    glyphsSelect.children('option:last-child').attr('selected', 'selected');
-    $('#glyphs img:last-child').addClass('visible');
+    glyphsSelect.children('option:first-child').attr('selected', 'selected');
+    $('#glyphs img:nth-child(2)').addClass('visible');
     glyphsSelect.on('change', function() {
       $('#glyphs img.visible').removeClass('visible');
       return $(this).parent('#glyphs').children('img.' + $(this).val()).addClass('visible');
