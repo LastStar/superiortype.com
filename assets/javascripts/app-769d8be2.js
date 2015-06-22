ff9ac5f31931302117e5c032386eed95c641fc48
Josef Pospíšil <josef.pospisil@laststar.eu>
2015-06-22 15:20:33 +0200

    Site updated to 2c1dc1c

--- assets/javascripts/app-769d8be2.js
+++ assets/javascripts/app-769d8be2.js
@@ -54,7 +54,7 @@ https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
 
 !function(){"use strict";function t(){}function e(t){this.options=i.Adapter.extend({},e.defaults,t),this.axis=this.options.horizontal?"horizontal":"vertical",this.waypoints=[],this.createWaypoints()}var i=window.Waypoint;e.prototype.createWaypoints=function(){for(var t={vertical:[{down:"enter",up:"exited",offset:"100%"},{down:"entered",up:"exit",offset:"bottom-in-view"},{down:"exit",up:"entered",offset:0},{down:"exited",up:"enter",offset:function(){return-this.adapter.outerHeight()}}],horizontal:[{right:"enter",left:"exited",offset:"100%"},{right:"entered",left:"exit",offset:"right-in-view"},{right:"exit",left:"entered",offset:0},{right:"exited",left:"enter",offset:function(){return-this.adapter.outerWidth()}}]},e=0,i=t[this.axis].length;i>e;e++){var o=t[this.axis][e];this.createWaypoint(o)}},e.prototype.createWaypoint=function(t){var e=this;this.waypoints.push(new i({element:this.options.element,handler:function(t){return function(i){e.options[t[i]].call(this,i)}}(t),offset:t.offset,horizontal:this.options.horizontal}))},e.prototype.destroy=function(){for(var t=0,e=this.waypoints.length;e>t;t++)this.waypoints[t].destroy();this.waypoints=[]},e.defaults={enter:t,entered:t,exit:t,exited:t},i.Inview=e}();
 (function() {
-  var addToWished, address, clearMessage, currentWished, defaultSpeed, detailsActive, detailsIn, emailIsValid, family, fixHeader, glyphsActive, glyphsIn, glyphsSelect, hideWishedBox, inWished, refreshWished, removeFromWished, removed, renderWished, showSlideShow, showWishedBox, style, styles, stylesActive, stylesGr, stylesIn, wishedBox, wishedClose, wishedSpan;
+  var addToWished, address, clearMessage, currentWished, defaultSpeed, detailsActive, detailsIn, emailIsValid, family, fixHeader, glyphsActive, glyphsIn, glyphsSelect, hideWishedBox, inWished, refreshWished, removeFromWished, removed, renderWished, showAddress, showSlideShow, showWishedBox, style, styles, stylesActive, stylesGr, stylesIn, wishedBox, wishedClose, wishedSpan;
 
   wishedSpan = $('#wished span');
 
@@ -582,17 +582,21 @@ https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
   if ($('address').length > 0) {
     address = $('address');
     removed = false;
-    address.addClass('visible');
-    $(window).on('scroll', function() {
-      if (!removed && $(window).scrollTop() > ($('address').position().top + $('address').height())) {
-        address.removeClass('visible');
-        return removed = true;
-      } else if (removed && $(window).scrollTop() < ($('address').position().top + $('address').height())) {
-        address.addClass('visible');
-        return removed = false;
-      }
-    });
+    showAddress = function() {
+      return address.addClass('visible');
+    };
+    setTimeout(showAddress, defaultSpeed);
   }
+
+  $(window).on('scroll', function() {
+    if (!removed && $(window).scrollTop() > ($('address').position().top + $('address').height())) {
+      address.removeClass('visible');
+      return removed = true;
+    } else if (removed && $(window).scrollTop() < ($('address').position().top + $('address').height())) {
+      address.addClass('visible');
+      return removed = false;
+    }
+  });
 
   if ($('select.glyphs').size() > 0) {
     glyphsSelect = $('select.glyphs');
