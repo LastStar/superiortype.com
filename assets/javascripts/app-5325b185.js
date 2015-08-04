887244eaf75307881fa73ea878d0f34bef2b7758
Josef Pospíšil <josef.pospisil@laststar.eu>
2015-08-04 10:30:06 +0200

    Site updated to 62f618f

--- assets/javascripts/app-5325b185.js
+++ assets/javascripts/app-5325b185.js
@@ -54,7 +54,7 @@ https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
 
 !function(){"use strict";function t(){}function e(t){this.options=i.Adapter.extend({},e.defaults,t),this.axis=this.options.horizontal?"horizontal":"vertical",this.waypoints=[],this.createWaypoints()}var i=window.Waypoint;e.prototype.createWaypoints=function(){for(var t={vertical:[{down:"enter",up:"exited",offset:"100%"},{down:"entered",up:"exit",offset:"bottom-in-view"},{down:"exit",up:"entered",offset:0},{down:"exited",up:"enter",offset:function(){return-this.adapter.outerHeight()}}],horizontal:[{right:"enter",left:"exited",offset:"100%"},{right:"entered",left:"exit",offset:"right-in-view"},{right:"exit",left:"entered",offset:0},{right:"exited",left:"enter",offset:function(){return-this.adapter.outerWidth()}}]},e=0,i=t[this.axis].length;i>e;e++){var o=t[this.axis][e];this.createWaypoint(o)}},e.prototype.createWaypoint=function(t){var e=this;this.waypoints.push(new i({element:this.options.element,handler:function(t){return function(i){e.options[t[i]].call(this,i)}}(t),offset:t.offset,horizontal:this.options.horizontal}))},e.prototype.destroy=function(){for(var t=0,e=this.waypoints.length;e>t;t++)this.waypoints[t].destroy();this.waypoints=[]},e.defaults={enter:t,entered:t,exit:t,exited:t},i.Inview=e}();
 (function() {
-  var addToWished, address, clearMessage, currentWished, defaultSpeed, detailsActive, detailsIn, emailIsValid, family, fixHeader, glyphsActive, glyphsIn, glyphsSelect, hideWishedBox, inWished, inuseActive, inuseCount, isMobile, makeHeaderFixed, menuOpened, refreshWished, removeFromWished, removed, renderWished, showAddress, showSlideShow, showWishedBox, style, styles, stylesActive, stylesGr, unmakeHeaderFixed, wishedBox, wishedClose, wishedSpan;
+  var addToWished, address, clearMessage, currentWished, defaultSpeed, detailsActive, detailsIn, emailIsValid, family, fixHeader, glyphsActive, glyphsIn, glyphsSelect, hideWishedBox, inWished, inuseActive, inuseCount, isMobile, isSafariFirst, makeHeaderFixed, menuOpened, refreshWished, removeFromWished, removed, renderWished, showAddress, showSlideShow, showWishedBox, style, styles, stylesActive, stylesGr, unmakeHeaderFixed, wishedBox, wishedClose, wishedSpan;
 
   wishedSpan = $('#wished span');
 
@@ -65,6 +65,8 @@ https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
   defaultSpeed = 250;
 
   isMobile = window.screen.availWidth < 750;
+
+  isSafariFirst = navigator.userAgent.indexOf('Safari') !== -1 && navigator.userAgent.indexOf('Chrome') === -1;
 
   emailIsValid = function(email) {
     var pattern;
@@ -266,6 +268,13 @@ https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
       svg.css({
         height: height
       });
+      if (isSafariFirst) {
+        isSafariFirst = false;
+        console.log('first dry');
+        $(window).resize();
+        showSlideShow();
+        return;
+      }
       zero = 'translate(0, 0)';
       scale = {
         initial: 0.0001,
