156f9667a3b4a4926504965e0cf501a5bf82e24c
Josef Pospíšil <josef.pospisil@laststar.eu>
2015-08-24 15:09:16 +0200

    Site updated to 8c9fdcd

--- assets/javascripts/app-23d8d558.js
+++ assets/javascripts/app-23d8d558.js
@@ -54,7 +54,7 @@ https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
 
 !function(){"use strict";function t(){}function e(t){this.options=i.Adapter.extend({},e.defaults,t),this.axis=this.options.horizontal?"horizontal":"vertical",this.waypoints=[],this.createWaypoints()}var i=window.Waypoint;e.prototype.createWaypoints=function(){for(var t={vertical:[{down:"enter",up:"exited",offset:"100%"},{down:"entered",up:"exit",offset:"bottom-in-view"},{down:"exit",up:"entered",offset:0},{down:"exited",up:"enter",offset:function(){return-this.adapter.outerHeight()}}],horizontal:[{right:"enter",left:"exited",offset:"100%"},{right:"entered",left:"exit",offset:"right-in-view"},{right:"exit",left:"entered",offset:0},{right:"exited",left:"enter",offset:function(){return-this.adapter.outerWidth()}}]},e=0,i=t[this.axis].length;i>e;e++){var o=t[this.axis][e];this.createWaypoint(o)}},e.prototype.createWaypoint=function(t){var e=this;this.waypoints.push(new i({element:this.options.element,handler:function(t){return function(i){e.options[t[i]].call(this,i)}}(t),offset:t.offset,horizontal:this.options.horizontal}))},e.prototype.destroy=function(){for(var t=0,e=this.waypoints.length;e>t;t++)this.waypoints[t].destroy();this.waypoints=[]},e.defaults={enter:t,entered:t,exit:t,exited:t},i.Inview=e}();
 (function() {
-  var addToWished, address, clearMessage, currentWished, defaultSpeed, detailsActive, detailsIn, emailIsValid, family, fixHeader, glyphsActive, glyphsIn, glyphsSelect, hideWishedBox, inWished, inuseActive, inuseCount, isMobile, isSafariFirst, makeHeaderFixed, menuOpened, refreshWished, removeFromWished, removed, renderWished, showAddress, showSlideShow, showWishedBox, style, styles, stylesActive, stylesGr, unmakeHeaderFixed, wishedBox, wishedClose, wishedSpan;
+  var addToWished, address, clearMessage, currentWished, defaultSpeed, emailIsValid, family, fixHeader, glyphsSelect, hideWishedBox, inWished, inuseCount, isMobile, isSafariFirst, makeHeaderFixed, menuOpened, refreshWished, removeFromWished, removed, renderWished, showAddress, showSlideShow, showWishedBox, style, styles, stylesGr, unmakeHeaderFixed, wishedBox, wishedClose, wishedSpan;
 
   wishedSpan = $('#wished span');
 
@@ -640,9 +640,31 @@ https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
       }
     });
     $('.wish').addClass('pushed');
-    $('#styles .styles').show(defaultSpeed, function() {
-      var showWish;
-      $('#styles .styles').addClass('visible');
+    $('.slide').show(defaultSpeed, function() {
+      var activate, deactivate, detailsActive, detailsIn, glyphsActive, glyphsIn, inuseActive, showWish, stylesActive;
+      deactivate = function() {
+        return $('a.active').removeClass('active');
+      };
+      activate = function(section) {
+        return $("a." + section).addClass('active');
+      };
+      inuseActive = function() {
+        deactivate();
+        return activate('inuse');
+      };
+      detailsActive = function() {
+        deactivate();
+        return activate('details');
+      };
+      glyphsActive = function() {
+        deactivate();
+        return activate('glyphs');
+      };
+      stylesActive = function() {
+        deactivate();
+        return activate('styles');
+      };
+      $('.slide').addClass('visible');
       if ($(window).scrollTop() < $('#styles .styles').height()) {
         stylesActive();
       }
@@ -659,49 +681,33 @@ https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
           return refreshWished(currentWished());
         }
       };
-      return setTimeout(showWish, 3 * defaultSpeed);
-    });
-    inuseActive = function() {
-      $('a.active').removeClass('active');
-      return $('a.inuse').addClass('active');
-    };
-    detailsActive = function() {
-      $('a.active').removeClass('active');
-      return $('a.details').addClass('active');
-    };
-    glyphsActive = function() {
-      $('a.active').removeClass('active');
-      return $('a.glyphs').addClass('active');
-    };
-    stylesActive = function() {
-      $('a.active').removeClass('active');
-      return $('a.styles').addClass('active');
-    };
-    detailsIn = new Waypoint.Inview({
-      element: $('#details')[0],
-      enter: function(direction) {
-        return detailsActive();
-      },
-      exited: function(direction) {
-        if (direction === 'up') {
+      setTimeout(showWish, 3 * defaultSpeed);
+      detailsIn = new Waypoint.Inview({
+        element: $('#details')[0],
+        enter: function(direction) {
+          return detailsActive();
+        },
+        exited: function(direction) {
+          if (direction === 'up') {
+            return glyphsActive();
+          } else {
+            return inuseActive();
+          }
+        }
+      });
+      return glyphsIn = new Waypoint.Inview({
+        element: $('#glyphs')[0],
+        enter: function(direction) {
           return glyphsActive();
-        } else {
-          return inuseActive();
+        },
+        exited: function(direction) {
+          if (direction === 'up') {
+            return stylesActive();
+          } else {
+            return detailsActive();
+          }
         }
-      }
-    });
-    glyphsIn = new Waypoint.Inview({
-      element: $('#glyphs')[0],
-      enter: function(direction) {
-        return glyphsActive();
-      },
-      exited: function(direction) {
-        if (direction === 'up') {
-          return stylesActive();
-        } else {
-          return detailsActive;
-        }
-      }
+      });
     });
   }
 
